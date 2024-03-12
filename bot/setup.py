c22eabe4dd5214bd888ec0857f3bf7a63a4cb7a1
nishchay~ <108711354+NishchayKQ@users.noreply.github.com>
2024-03-12 23:03:06 +0530

    cleaning up the code

---
+++ bot/setup.py
@@ -0,0 +1,13 @@
+import discord
+
+# List of owner IDs
+owner: list[discord.User.id] = []
+
+# id of servers you want the bot to work in
+listOfServers: list[discord.Guild.id] = []
+
+# Channel ID the bot will post if it's online!
+channelID: discord.TextChannel.id = 0
+
+# add your token
+token: str = ""
